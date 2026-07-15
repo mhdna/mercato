1c7b055680e47a4acef79f29d4a29a7fac2139e6
Mahdi <mhdna@outlook.com>
2026-07-15 05:31:57 +0300

    docs: use statik to embed swaggerui docs directly into the binary

--- main.go
+++ main.go
@@ -11,9 +11,11 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/mhdna/kashi/api"
 	db "github.com/mhdna/kashi/db/sqlc"
+	_ "github.com/mhdna/kashi/doc/statik"
 	"github.com/mhdna/kashi/gapi"
 	"github.com/mhdna/kashi/pb"
 	"github.com/mhdna/kashi/util"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -89,8 +91,12 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	statikFs, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs", err)
+	}
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFs))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
